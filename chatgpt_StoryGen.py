68b48ab0e8f9b62691dcde4f37900e65ae6c213b
MaryamDu <98149297+MaryamDu@users.noreply.github.com>
2023-11-16 21:00:30 -0800

    Does its own thing but a general basis for the code maybe?

--- chatgpt_StoryGen.py
+++ chatgpt_StoryGen.py
@@ -1,7 +1,6 @@
 import openai
 import json
 
-
 file = open("fantasy-solution.pddl")
 
 content = file.readlines()
@@ -26,24 +25,26 @@ while i != count:
 
 for pos, lineNum in enumerate(content):
     if pos in lines:
-        prompt = lineNum
+        prompt = "Continue the chapter based off the previous one and the next step defined by the following: " + lineNum
         #print(prompt) 
 
 file.close()
 
-postprompt = "Write a chapter based story from the first chapter to chapter number " + str(count) + "based on the following steps where each line is a new chapter that only lasts a few paragraphs: " + prompt
+postprompt = "Write a chapter based story from the first chapter to chapter number " + str(count) + "based on the following steps where each line is a new chapter: " #+ prompt
 
 
 completion = openai.ChatCompletion.create(
   model = "gpt-3.5-turbo",
   temperature = 0.8, #degree of randomness between 0 and 2
   max_tokens = 2000,
-  messages = [
-    {"role": "system", "content": "You are a story teller."},
-    {"role": "user", "content": postprompt},
-    {"role": "assistant", "content": "Rory confesses exuberantly, \"I want to spend the rest of my life with you! Let's go on an adventure together!\""},
-  ]
-)
+  messages = 
+    [{"role": "system", "content": "You are a story teller."}] 
+    + [{"role": "user", "content" : postprompt}]
+    + [{"role": "user", "content": line} for line in prompt]
+    + [{"role": "assistant", "content": "Rory confesses exuberantly, \"I want to spend the rest of my life with you!\""}]
+    + [{"role": "assistant", "content": "In a statement like (accept talia rory village), the first word is the action, the second the character performing the action, the third the character receiving the action, and the fourth is the current location."}]
+    + [{"role": "assistant", "content": "In a statement like (travel rory village cave), the first word is the action, the second the character performing the action, the third is the origin point, and the fourth is the destination."}]
+    )
 
 output = open("story.txt", "w")
 stroutput = str(completion["choices"][0].message["content"])
